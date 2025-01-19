68a6aed3d3e8d063e5a5bdbd449e6f9233ac08c1
radogalev <163414180+radogalev@users.noreply.github.com>
2025-01-19 19:58:42 +0200

    Made some changes to file location

--- smmp/rvideo/views.py
+++ smmp/rvideo/views.py
@@ -5,16 +5,13 @@ from io import BytesIO
 from PIL import Image
 from together import Together
 from supabase import create_client, Client
+from . import script_gen_engine
 
 
 SUPABASE_URL = "https://lnitjdoumoecovyrmdgi.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuaXRqZG91bW9lY292eXJtZGdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyNzk3ODEsImV4cCI6MjA1Mjg1NTc4MX0.TG4eJS00hHnqdVHc2tRp0Lyr3GO75KDjLL3cTsSONvA"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-
-def home(request):
-   return render(request, 'index.html')
 
 def dashboard(request): 
      
@@ -29,79 +26,8 @@ def dashboard(request):
         tone = request.POST.get('tone')
 
         if user_input and video_length and tone:
-                genai.configure(api_key="AIzaSyBWrOq65UuHcogPJbS0Rzu3sgnmZcKS6Mw")
-
-                model = genai.GenerativeModel("gemini-1.5-flash")
-                prompt = f"""
-                You are a model specialized in creating scripts for short-form videos that will be used on platforms like TikTok, Instagram Reels, and YouTube Shorts.
-                Here is the user input: {user_input}. Based on the user input and the video length: {video_length}s, create a script to be added as a voiceover to the video.
-                Ensure the script aligns with the tone: {tone}. Do not add any additional details only and only the exact words
-                that the narrator has to say.
-                """
-                script_response = model.generate_content(prompt)
-                script_text = script_response.text
-
-                full_segment_text = model.generate_content(f"""sumary_line
-                You are a model specialized in creating segments out of a given script.You have to analyze the script and brake it down into smaller segments based on context and what the
-                narrator is talking about. IT IS ABSOLUTELY FORBIDDEN TO CHANGE ANY TEXT OF THE ORIGINAL SCRIPT WHATSOEVER. YOU ONLY HAVE TO ANALYZE IT AND CUT IT INTO MULTIPLE PARTS.
-                This is the script: {script_text}
-                Each segment should start with "#" and end with "*".It has to be enloclosed with "#" and "*".
-                """)
-                
-                full_segmented_text = full_segment_text.text
-
-                segments = [""]
-                current_segment = ""
-                inside_segment = False
-
-                for char in full_segmented_text:
-                    if char == "#": 
-                       inside_segment = True
-                       current_segment = ""
-                    elif char == "*":  
-                        if inside_segment:
-                           segments.append(current_segment.strip())
-                           inside_segment = False
-                    elif inside_segment:
-                        current_segment += char
- 
-                
-                client = Together()
-                image_generation_prompt = ""
-
-                image_gen_prompts = ['']
-                for segment in segments:
-                    if segment != "":
-                        image_generation_prompt = model.generate_content(f'''
-                        You are a model that is spezialized in creating image generation prompts based on a given text.
-                        The prompt should be very descriptive and as detailed as possible for the ai to understand.
-                        This is the text: {segment}. Do not add any maps or text to the images whatsoever.
-                        You also need to take into account the tone of the whole text which is given here: {tone}
-                    ''')
-                    image_gen_prompts.append(image_generation_prompt)
-
-                print(image_gen_prompts)
-                 
-                for image_gen_prompt in image_gen_prompts:
-                    if image_gen_prompt != "":
-                         print(image_gen_prompt)
-                         response = client.images.generate(
-                         prompt=f"{image_gen_prompt} Do not add any maps or text to the images whatsoever.The image has to be continous and connected.It cant look like it is comprised of multiple images",
-                         model="black-forest-labs/FLUX.1-schnell-Free",
-                         width=1008,
-                         height=1792,
-                         steps=1,
-                         n=1,
-                         response_format="b64_json"
-                         )
+            script_gen_engine.script_generation(user_input, video_length, tone, request, session_username)
         
-                         b64_image = response.data[0].b64_json
-                         image_data = base64.b64decode(b64_image)
-                         image = Image.open(BytesIO(image_data))
-                         image.show()
-
- 
-                return render(request, 'dashboard.html', {'script': script_text, 'segments': full_segmented_text, 'edited_segments': segments, 'username' :  session_username})
         else:
             return render(request, 'dashboard.html', {'error': 'Please fill all fields'})
      else:
@@ -131,7 +57,6 @@ def log_in(request):
                 return render(request, 'login.html', {'error': 'Invalid username or password'})
         else:
             return render(request, 'login.html', {'error': 'Invalid username or password'})
-    
 
     return render(request, 'login.html')
 
